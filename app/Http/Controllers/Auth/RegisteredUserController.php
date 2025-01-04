cf72eeea78c0ce2bf8dcc516de3759cb88e5423b
lyduybach2612 <lyduybach900@gmail.com>
2025-01-04 23:54:56 +0700

    first commit

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
+            'password' => ['required'],
+            'phoneNumber' => ['required', 'string', 'max:15'],
+            'username' => ['required', 'string', 'max:50'],
+        ]);
+
+        $user = User::create([
+            'fullname' => $request->name,
+            'username' => $request->username,
+            'address' => $request->address,
+            'phone_number' => $request->phoneNumber,
+            'password' => Hash::make($request->password),
+            'role_name' => 'customer',
+        ]);
+
+        event(new Registered($user));
+
+
+        return redirect(route('login', absolute: false));
+    }
+}
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.auth')
+@section('content')
+<div class="container">
+    <h1 class="text-center mb-3 text-primary">Register</h1>
+    <form method="post" action="{{ route(name: 'register') }}">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" name="name" id="name">
+        </div>
+        <div class="mb-3">
+            <label for="address" class="form-label">Address</label>
+            <input type="text" class="form-control" name="address" id="address">
+        </div>
+        <div class="mb-3">
+            <label for="phone" class="form-label">Phone</label>
+            <input type="text" class="form-control" name="phoneNumber" id="phone">
+        </div>
+        <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" class="form-control" name="username" id="username">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" id="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+@endsection
