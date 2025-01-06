e858e6fa493c178cca454ff4cbaadb88bdcdb2c2
vuongchienn <vuongtatchien21122004@gmail.com>
2025-01-06 13:47:22 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('/');
+
+
 
 require __DIR__.'/auth.php';
